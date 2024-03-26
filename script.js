bcf5c0fa95bc90a5c38010c2e2931e2dd525d81f
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-26 17:39:56 +0400

    add: ingredients data and styles

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ let userInp = document.getElementById("user-inp").value;
 fetch(url + "big mac")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     let myMeal = data.meals[0];
     let count = 1;
     let ingredients = [];
     for (let i in myMeal) {
       let ingredient = "";
       let measure = "";
-      if (i.startsWith("strIngridient") && myMeal[i]) {
+      if (i.startsWith("strIngredient") && myMeal[i]) {
         ingredient = myMeal[i];
         measure = myMeal[`strMeasure` + count];
         count += 1;
         ingredients.push(`${measure} ${ingredient}`);
       }
     }
+    console.log(ingredients);
 
     result.innerHTML = `
     <img src=${myMeal.strMealThumb}>
@@ -28,10 +28,23 @@ fetch(url + "big mac")
         <h2>${myMeal.strMeal}</h2>
         <h4>${myMeal.strArea}</h4>
     </div>
-    <div id="igredient-con"></div>
+    <div id="ingredient-con"></div>
     <div id="recipe">
        <button id="hide-recipe">X</button>
        <pre id="instructions">${myMeal.strInstructions}</pre>
     </div>
+    <button id="show-recipe">View Recipe</button>
     `;
+    let ingredientCon = document.getElementById("ingredient-con");
+    let parent = document.createElement("ul");
+    let recipe = document.getElementById("recipe");
+    let hideRecipe = document.getElementById("hide-recipe");
+    let showRecipe = document.getElementById("show-recipe");
+
+    ingredients.forEach((i) => {
+      let child = document.createElement("li");
+      child.innerText = i;
+      parent.appendChild(child);
+      ingredientCon.appendChild(parent);
+    });
   });
